9b1ef7807a2ad17b7d86581ad46ba17f967cae85
felipe.felix <felipe.felix@danone.com>
2026-02-10 12:23:39 -0700

    chore: change namespace and version to 21.1.0

--- src/app/demo-datetime/demo-datetime.component.ts
+++ src/app/demo-datetime/demo-datetime.component.ts
@@ -82,7 +82,7 @@ export class DemoDatetimeComponent {
   public stepMinutes = [1, 5, 10, 15, 20, 25];
   public stepSeconds = [1, 5, 10, 15, 20, 25];
 
-  public code1 = 'npm install --save @ngxmc/datetime-picker';
+  public code1 = 'npm install --save @ngx-mce/datetime-picker';
 
   public code3 = `
   <mat-form-field>
@@ -101,7 +101,7 @@ export class DemoDatetimeComponent {
     NgxMatDatetimePickerComponent,
     NgxMatNativeDateComponent,
     NgxMatTimepickerComponent
-  } from '@ngxmc/datetime-picker';
+  } from '@ngx-mce/datetime-picker';
 
   @Component({
     imports: [
