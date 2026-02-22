e0dcafff17f3eb036c469ffacd250c32511759bd
Felipe BF <fprgw32@gmail.com>
2026-02-22 13:46:29 -0700

    chore: remove everything prior to rebase

--- projects/color-picker/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of color-picker
- */
-export * from './lib/components';
-export * from './lib/directives';
-export * from './lib/helpers';
-export * from './lib/models';
-export * from './lib/services';
