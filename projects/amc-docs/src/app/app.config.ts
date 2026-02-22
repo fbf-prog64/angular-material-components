129bbfb1f3aab5cd72d0b857e717d118d82c5b28
Felipe BF <fprgw32@gmail.com>
2026-02-22 12:06:14 -0700

    feat: implement app docs demo

--- projects/amc-docs/src/app/app.config.ts
+++ projects/amc-docs/src/app/app.config.ts
@@ -1,10 +1,15 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideNativeDateAdapter(),
+    provideAnimations(),
+    provideZoneChangeDetection(),
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes)
   ]
